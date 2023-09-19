56b322059e48c47753ddbaee2ea7dda061964fdf
sari-harin <harinqq@gmail.com>
2023-09-19 22:34:29 +0900

    [ 우선순위 큐 ] 9월 19일

---
+++ 05_우선순위_큐/2075.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, num;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    cin >> n;
+    
+    for(int i=0;i<n*n;i++){
+            cin >> num;
+            pq.push(num);
+            if(pq.size() > n){
+                pq.pop();
+            }
+    }
+    
+    cout << pq.top();
+
+    return 0;
+}
